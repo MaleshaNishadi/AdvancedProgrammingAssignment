8f105623289bd6d09d174c8e0b1b864bc594de6c
MaleshaNishadi <malesharajapaksha637@gmail.com>
2026-07-07 20:55:21 +0530

    Student

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+
+    public Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void displayStudent() {
+        System.out.println("Student ID : " + id);
+        System.out.println("Student Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Course : " + course);
+    }
+}
